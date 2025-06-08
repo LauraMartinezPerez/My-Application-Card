2d9b67fb081a64d77cb95bb290ee7111b0e70b46
Laura <laura.laukiz@gmail.com>
2025-06-08 13:25:25 +0200

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,2 @@
-
+const c=document.querySelectorAll(".nav__item");function i(){c.forEach(t=>t.classList.remove("active")),this.classList.add("active"),this.classList.add("active")}c.forEach(t=>t.addEventListener("click",i));
 //# sourceMappingURL=main.js.map
