11347c7018e8598be44402659eff0188f06ee53b
Laura <laura.laukiz@gmail.com>
2025-06-08 13:25:12 +0200

    share nav bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 
+const $list = document.querySelectorAll('.nav__item');
+
+function activeLink () {
+    $list.forEach(link => link.classList.remove('active'));
+    this.classList.add('active');
+    this.classList.add('active');
+}
+
+$list.forEach(link => link.addEventListener('click', activeLink));
