1674086aa861df9c3af8bcc04a1b09c5ec510f32
Laura <laura.laukiz@gmail.com>
2025-06-09 09:53:43 +0200

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,2 @@
-const t=document.querySelectorAll(".nav__item");function c(){t.forEach(e=>e.classList.remove("active")),this.classList.add("active")}t.forEach(e=>{e.addEventListener("mouseenter",c)});
+
 //# sourceMappingURL=main.js.map
