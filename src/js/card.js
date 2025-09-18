0d529f1e01e02a57a307cac31ceb547085c862da
Laura <laura.laukiz@gmail.com>
2025-09-18 13:34:41 +0200

    mantener datos de card al volver al form

--- src/js/card.js
+++ src/js/card.js
@@ -17,13 +17,15 @@ const technologiesCard = document.querySelector(".js_preview_technologies");
 const linkedinCard = document.querySelector(".js_preview_linkedin");
 const bgPreview = document.querySelector(".js-bg-preview");
 
+const backLink = document.querySelector(".js-backLink");
+
 
 //Obtener el id de la url
 const urlParam = new URLSearchParams(window.location.search);
 const id = urlParam.get("id");
 console.log(id);
 
-fetch(`https://dev.adalab.es/api/info/${id}`)
+/* fetch(`https://dev.adalab.es/api/info/${id}`)
     .then(response => response.json())
     .then(data => {
         console.log("esto es data de GET",data);
@@ -53,5 +55,49 @@ fetch(`https://dev.adalab.es/api/info/${id}`)
             bgPreview.classList.remove("bg-orange");
             bgPreview.classList.add("bg-green");
         }
-        
+        console.log("esto es targetData", targetData);
     })
+ */
+if (id) {
+  /* --- FETCH PARA OBTENER DATOS DE LA TARJETA --- */
+  fetch(`https://dev.adalab.es/api/info/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      const targetData = data.data;
+
+      // Rellenar preview
+      nameCard.innerText = targetData.field2;
+      cityCard.innerText = targetData.field3;
+      emailCard.innerText = targetData.field4;
+      trainingCard.innerText = targetData.field5;
+      skillsCard.innerText = targetData.field6;
+      technologiesCard.innerText = targetData.field7;
+      linkedinCard.innerText = targetData.field8;
+      motivationCard.innerText = targetData.field9;
+      photoCard.src = targetData.photo;
+
+      // Foto
+
+
+      // Fondo según field1
+      if (targetData.field1 === 1) {
+        bgPreview.classList.add("bg-grey");
+        bgPreview.classList.remove("bg-orange", "bg-green");
+      } else if (targetData.field1 === 2) {
+        bgPreview.classList.add("bg-orange");
+        bgPreview.classList.remove("bg-grey", "bg-green");
+      } else if (targetData.field1 === 3) {
+        bgPreview.classList.add("bg-green");
+        bgPreview.classList.remove("bg-grey", "bg-orange");
+      }
+
+      // Configurar botón ATRÁS dinámicamente
+      backLink.href = `./forms.html?cardId=${id}`;
+    })
+    .catch(err => console.error("Error al recuperar tarjeta:", err));
+} else {
+  // Si no hay id, el botón ATRÁS apunta al formulario sin id
+  backLink.href = "./forms.html";
+}
+
+    
