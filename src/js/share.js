ab7bd906e17c89287f9acbb81a44d048793ff16a
Laura <laura.laukiz@gmail.com>
2025-09-22 12:42:48 +0200

    limpiando comments

--- src/js/share.js
+++ src/js/share.js
@@ -1,26 +1,3 @@
-/* "use strict";
-
-// Selecciona elementos del DOM
-const whatsappBtn = document.querySelector(".js-whatsapp-icon");
-
-// Función para compartir en WhatsApp
-const handleShareWhatsapp = () => {
-    
-     // Link directo a la tarjeta con el id
-    const link = `https://lauramartinezperez.github.io/My-Application-Card/card.html?id=${cardId}`;
-
- console.log("Ha hecho click en icono whatsapp", link);
-
-    const message = `Comparto con ustedes mi tarjeta CV personalizada: ${link}`;
-    const whatsappURL = `https://api.whatsapp.com/send?text=${encodeURIComponent(message)}`;
-
-    // Abrir WhatsApp en nueva pestaña/ventana
-    window.open(whatsappURL, "_blank"); 
-};
-
-whatsappBtn.addEventListener("click", handleShareWhatsapp);
- */
-
 "use strict";
 
 const whatsappBtn = document.querySelector(".js-whatsapp-icon");
